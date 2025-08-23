7fb1c29998b6c9e7ebf73e88f0cfa64e8ced3dde
shanye-west <shanepeterson32@gmail.com>
2025-08-23 04:12:55 +0000

    idk

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ab4816ec'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/offline.html",
-    "revision": "0.pnr4tpg7mno"
+    "revision": "0.2t2sd1rphdg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/offline.html"), {
